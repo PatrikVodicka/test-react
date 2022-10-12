67f4f4b3c6cc78ee4813c11b489b37b9c7a410ae
Patrik Vodička <vodicka.patrik@gmail.com>
2022-10-12 13:52:07 +0200

    react test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useState} from 'react'
 
 function App() {
+  const [markCoords, setMarkCoords] = useState([]);
+
+  function setMark (event) {
+    const coordXPercnt = 100 / event.view.innerWidth * event.pageX
+    const coordYPercnt = 100 / event.view.innerHeight * event.pageY
+    setMarkCoords(current => [...current, {
+      X: coordXPercnt,
+      Y: coordYPercnt,
+    }])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="canvas"
+      onClick={setMark}
+    >
+      {markCoords.map((coord, index) => {
+        return (
+          <div
+            key={index}
+            onClick={() => setMarkCoords([])}
+            style={{
+              position: 'absolute',
+              top: coord.Y + '%',
+              left: coord.X + '%',
+              width: 10,
+              height: 10,
+              margin: '-5px 0 0 -5px',
+              background: 'red',
+              borderRadius: '50%',
+            }}
+          />
+        );
+      })}
     </div>
   );
 }
